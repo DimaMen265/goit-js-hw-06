1c07b57892b6a964f76888eb5d5dd542326d4f7f
DimaMen265 <menzak87@gmail.com>
2023-09-14 15:53:05 +0200

    The end task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach(ingredient => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = ingredient;
+  ingredientItem.classList.add("item");
+  ingredientsList.appendChild(ingredientItem);
+})
