0fa09606ba854431b1cb5c35ff49937c660e8c9f
DimaMen265 <menzak87@gmail.com>
2023-09-16 20:18:49 +0200

    The end task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector(".login-form");
 
+const getSubmit = event => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (email === "" || password === "") {
+        return alert("ERROR!!! Please fill in all fields!");
+    };
+    
+    const loginValues = {
+        email,
+        password,
+    };
+
+    console.log(loginValues);
+
+    form.reset();
+};
+
+loginForm.addEventListener("submit", getSubmit);
